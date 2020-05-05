cfab137c3896f020c8f45e1d974b5d738c811088
Sharlon Regales <sharlonregales+dev@gmail.com>
2020-05-05 02:02:21 +0200

    add missing test

--- src/shorten/tests/test_factory.py
+++ src/shorten/tests/test_factory.py
@@ -1,13 +1,18 @@
 import pytest
 
 from . import URL, URL_SHORT
-from .factory import ShortCodeUrlFactory
+from .factory import ShortCodeUrlFactory, UrlFactory
 from ..models import Url
 
 
-@pytest.mark.django_db
-def test_url_factory(db) -> None:
-    assert len(Url.objects.all()) == 0 
+def test_url_shortcode_factory(db) -> None:
     url = ShortCodeUrlFactory()
     assert Url.objects.filter(pk=url.pk).exists()
     assert url.shortcode == URL_SHORT
+
+
+def test_url_factory(db) -> None:
+    assert len(Url.objects.all()) == 0 
+    url = UrlFactory()
+    assert Url.objects.filter(pk=url.pk).exists()
+    assert url.shortcode != URL_SHORT
